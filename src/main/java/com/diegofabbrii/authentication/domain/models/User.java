70a0e1f12efbd6b5a98839b5e70521510eaff772
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-06-23 17:01:53 -0300

    feat: add User entity

---
+++ src/main/java/com/diegofabbrii/authentication/domain/models/User.java
@@ -0,0 +1,22 @@
+package com.diegofabbrii.authentication.domain.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+	private String username;
+	private String email;
+	private String password;
+
+}
