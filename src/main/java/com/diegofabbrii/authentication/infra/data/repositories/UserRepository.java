1d37b3c81916f870abd111f8a9cd671a2cbdbb92
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-06-24 01:16:58 -0300

    feat: add findByEmail and findByusername methods in UserRepository

--- src/main/java/com/diegofabbrii/authentication/infra/data/repositories/UserRepository.java
+++ src/main/java/com/diegofabbrii/authentication/infra/data/repositories/UserRepository.java
@@ -3,5 +3,11 @@ package com.diegofabbrii.authentication.infra.data.repositories;
 import com.diegofabbrii.authentication.domain.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+	
+	Optional<User> findByEmail(String email);
+	Optional<User> findByUsername(String username);
+	
 }
