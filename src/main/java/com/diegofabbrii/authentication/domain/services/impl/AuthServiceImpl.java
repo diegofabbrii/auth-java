85136039b6388f542baeb1be82526ffdeb49eaee
Diego Fabbri <diegofabbri.dev@gmail.com>
2025-06-26 16:52:34 -0300

    feat: add signIn method in AuthService

--- src/main/java/com/diegofabbrii/authentication/domain/services/impl/AuthServiceImpl.java
+++ src/main/java/com/diegofabbrii/authentication/domain/services/impl/AuthServiceImpl.java
@@ -1,11 +1,15 @@
 package com.diegofabbrii.authentication.domain.services.impl;
 
+import com.diegofabbrii.authentication.domain.dtos.auth.SignInRequestDTO;
 import com.diegofabbrii.authentication.domain.dtos.auth.SignUpRequestDTO;
 import com.diegofabbrii.authentication.domain.enums.UserRoleEnum;
 import com.diegofabbrii.authentication.domain.exceptions.auth.UserAlreadyExistsException;
 import com.diegofabbrii.authentication.domain.models.User;
 import com.diegofabbrii.authentication.domain.services.interfaces.AuthService;
 import com.diegofabbrii.authentication.infra.data.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,9 +18,14 @@ import java.util.Optional;
 public class AuthServiceImpl implements AuthService {
 
 	private final UserRepository _userRepository;
+	private final AuthenticationManager _authenticationManager;
 
-	public AuthServiceImpl(UserRepository userRepository) {
+	public AuthServiceImpl(
+			UserRepository userRepository,
+			AuthenticationManager authenticationManager
+	) {
 		_userRepository = userRepository;
+		_authenticationManager = authenticationManager;
 	}
 
 	@Override
@@ -31,14 +40,27 @@ public class AuthServiceImpl implements AuthService {
 		if (existedEmail.isPresent())
 			throw new UserAlreadyExistsException();
 			
+		String passwordHash = new BCryptPasswordEncoder()
+			.encode(signUpRequestDTO.password());
+		
 		User createdUser = new User();
 		
 		createdUser.setUsername(signUpRequestDTO.username());
 		createdUser.setEmail(signUpRequestDTO.email());
-		createdUser.setPassword(signUpRequestDTO.password());
+		createdUser.setPassword(passwordHash);
 		createdUser.setRole(UserRoleEnum.USER);
 		
 		_userRepository.save(createdUser);
 	}
-
+	
+	@Override
+	public void signIn(SignInRequestDTO signInRequestDTO) {
+		var usernamePassowrd = new UsernamePasswordAuthenticationToken(
+			signInRequestDTO.username(),
+			signInRequestDTO.password()
+		);
+		
+		_authenticationManager.authenticate(usernamePassowrd);
+	}
+	
 }
--- src/main/java/com/diegofabbrii/authentication/domain/services/interfaces/AuthService.java
+++ src/main/java/com/diegofabbrii/authentication/domain/services/interfaces/AuthService.java
@@ -1,9 +1,11 @@
 package com.diegofabbrii.authentication.domain.services.interfaces;
 
+import com.diegofabbrii.authentication.domain.dtos.auth.SignInRequestDTO;
 import com.diegofabbrii.authentication.domain.dtos.auth.SignUpRequestDTO;
 
 public interface AuthService {
 
 	void signUp(SignUpRequestDTO signUpRequestDTO);
+	void signIn(SignInRequestDTO signInRequestDTO);
 	
 }
